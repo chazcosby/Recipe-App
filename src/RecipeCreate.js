8aa7fbceaff2503c812b8925c4a033e066291de7
Chaz Cosby <93302320+chazcosby@users.noreply.github.com>
2022-04-23 01:24:06 -0500

    Update RecipeCreate.js

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -19,9 +19,7 @@ export default function RecipeCreate({ recipes, setRecipes }) {
     setRecipes([...recipes, form]);
     setForm({ ...initialFormState });
   };
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
+
 
   return (
     <form name="create" onSubmit={handleSubmit}>
